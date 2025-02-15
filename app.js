fb4edcbc7a8641b24fc7c86e2d2ec916dfd46bf4
Jeremy <kwozi76@gmail.com>
2025-02-15 14:54:46 +1300

    Updated message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('How much wood could a wood-chuck chuck, if a wood-chuck could chuck wood?'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
